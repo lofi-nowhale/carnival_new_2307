03e4a51024a0fd09174d48470744264900d3ea1a
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-07-31 09:31:15 -0600

    test: Add a test block for the tall_enough? method

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -29,6 +29,16 @@ RSpec.describe Visitor do
     visitor1.add_preference(:thrilling)
 
     expect(visitor1.preferences).to eq([:gentle, :thrilling])
+  end
 
+  it 'can tell if a visitor is tall enough for the ride based on the specified height' do 
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    visitor2 = Visitor.new('Tucker', 36, '$5')
+    visitor3 = Visitor.new('Penny', 64, '$15')
+
+    expect(visitor1.tall_enough?(54)).to eq(true)
+    expect(visitor2.tall_enough?(54)).to eq(false)
+    expect(visitor3.tall_enough?(54)).to eq(true)
+    expect(visitor1.tall_enough?(64)).to eq(false)
   end
 end
