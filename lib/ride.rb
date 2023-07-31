f28868bb3d97e21333b51126ad68f3e5a0ad472a
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-07-31 09:45:39 -0600

    feat: Build out the start of the Rider class

---
+++ lib/ride.rb
@@ -0,0 +1,11 @@
+class Ride
+  attr_reader :admission_fee, :excitement, :min_height, :name, :rider_log, :total_revenue
+  def initialize(ride_details)
+    @admission_fee = ride_details[:admission_fee]
+    @excitement = ride_details[:excitement]
+    @min_height = ride_details[:min_height]
+    @name = ride_details[:name]
+    @rider_log = {}
+    @total_revenue = 0
+  end
+end
