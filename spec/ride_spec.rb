a6e9cad8a89fdaefb38781f5f0d43ca039cefc91
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-08-01 20:14:59 -0600

    test: Write first base tests for the ride class

---
+++ spec/ride_spec.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+require './lib/visitor'
+require './lib/ride'
+
+RSpec.describe Ride do 
+  it 'exists' do 
+    ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+
+    expect(ride1).to be_a(Ride)
+  end
+
+  it 'has readable attributes' do 
+    ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+
+   expect(ride1.name).to eq("Carousel")
+   expect(ride1.min_height).to eq(24)
+   expect(ride1.admission_fee).to eq(1)
+   expect(ride1.excitement).to eq(:gentle)
+   expect(ride1.total_revenue).to eq(0)   
+  end
+end
